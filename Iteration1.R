78ae5b5c7d19a02d9816807e0a2464062d8acc6d
Nathan Lang <nathan.lang@marquette.edu>
2018-05-05 14:03:07 -0500

    restructure files and update

--- Iteration1.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-
-rawData <- read.csv("CDC_Salmonella_Data.csv")
-colnames(rawData)[1] <- "Year"
-
-qplot(Year, Illnesses, data = rawData, facets = Month~.)
---
+++ code/Cloro of Hospitalizations.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(fiftystater)
+
+df_state$Latitude <- NULL
+df_state$Longitude <- NULL
+
+df_state <- read.csv("DataScience/Project/Salmonella-Data-Analysis/csvs/df_state.csv")
+data("fifty_states")
+
+# map_id creates the aesthetic mapping to the state name column in your data
+p <- ggplot(df_state, aes(map_id = State)) + 
+  # map points to the fifty_states shape data
+  geom_map(aes(fill = Hospitalizations), map = fifty_states) + 
+  expand_limits(x = fifty_states$long, y = fifty_states$lat) +
+  coord_map() +
+  borders(
+    database = "state",
+    colour = "grey60") +
+  ggtitle("Count of Hospitalizations from Salmonella Illnesses (2009-2016)") +
+  scale_fill_gradient(
+    low = "yellow",
+    high = "red") +
+  scale_x_continuous(breaks = NULL) + 
+  scale_y_continuous(breaks = NULL) +
+  labs(x = "", y = "") +
+  theme(legend.position = "bottom", 
+        panel.background = element_rect(colour = 'red'))
+
+p
